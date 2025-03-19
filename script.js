f99e5c12593c9765b779a5a162839240d989fcdc
samuelhenrique1234-ops <samuel_moraes@estudante.sesisenai.org.br>
2025-03-19 10:03:55 -0300

    Atividade 2 Calculadora

--- script.js
+++ script.js
@@ -1,11 +1,18 @@
-function verificarPar(numero){
-    if(numero % 2 === 0) {
-        console.log ("par");
-    }else{
-        console.log("impar");
-    }
+function calcular (n1,n2){
+
+    const soma = n1 + n2;
+    const subtracao = n1 - n2;
+    const divisao = n2 !== 0 ? n1 / n2: 'Não pode dividir por zero';
+    const multiplicacao = n1 * n2;
+
+    console.log(soma);
+    console.log(subtracao);
+    console.log(divisao);
+    console.log(multiplicacao);
+
 }
 
+    let n1 = parseFloat(prompt("Digite seu primeiro número"));
+    let n2 = parseFloat(prompt("Digite seu segundo número"));
 
-verificarPar(4);
-verificarPar(7);
+    calcular (n1,n2); 
