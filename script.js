d7b1dfc664f41c6cc2543b4b6ec92eb297aaa6ad
samuelhenrique1234-ops <samuel_moraes@estudante.sesisenai.org.br>
2025-03-19 10:12:08 -0300

    Atividade 3

--- script.js
+++ script.js
@@ -1,18 +1,7 @@
-function calcular (n1,n2){
+let numero = 10;
+let delay = 0;
 
-    const soma = n1 + n2;
-    const subtracao = n1 - n2;
-    const divisao = n2 !== 0 ? n1 / n2: 'Não pode dividir por zero';
-    const multiplicacao = n1 * n2;
-
-    console.log(soma);
-    console.log(subtracao);
-    console.log(divisao);
-    console.log(multiplicacao);
-
-}
-
-    let n1 = parseFloat(prompt("Digite seu primeiro número"));
-    let n2 = parseFloat(prompt("Digite seu segundo número"));
-
-    calcular (n1,n2); 
+while (numero >=1){
+    console.log(numero);
+    numero--;
+} 
