8c1f6b0a6977538f80d13320ab815e6c20fe0be3
samuelhenrique1234-ops <samuel_moraes@estudante.sesisenai.org.br>
2025-03-26 09:44:48 -0300

    Atividade 6

--- script.js
+++ script.js
@@ -1,3 +1,10 @@
-function contarCaracteres (string) {
-    return string.length;
-}
+let carro = {
+    modelo: "Toro",
+    marca: "Fiat",
+    ano: 2018,
+    saudacao: function() {
+        console.log("O modelo do carro é " + this.modelo)
+    }
+};
+
+carro.saudacao();
